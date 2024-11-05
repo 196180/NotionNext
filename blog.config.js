12e4313df14f59d39959f3c6b580a4ede8c08c96
196180 <152402505+196180@users.noreply.github.com>
2024-11-05 23:49:07 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -276,8 +276,8 @@ const BLOG = {
     'https://cdn1.tianli0.top/gh/zhheo/Post-Abstract-AI@0.15.2/tianli_gpt.css',
   TianliGPT_JS:
     process.env.NEXT_PUBLIC_TIANLI_GPT_JS ||
-    'https://cdn1.tianli0.top/gh/zhheo/Post-Abstract-AI@0.15.2/tianli_gpt.js',
-  TianliGPT_KEY: process.env.NEXT_PUBLIC_TIANLI_GPT_KEY || '',
+    'https://jsd.cdn.zzko.cn/gh/196180/Post-Abstract-AI-NotionNext@master/pastking_gpt.js',
+  TianliGPT_KEY: process.env.NEXT_PUBLIC_TIANLI_GPT_KEY || 'notionai',
 
   // Chatbase 是否显示chatbase机器人 https://www.chatbase.co/
   CHATBASE_ID: process.env.NEXT_PUBLIC_CHATBASE_ID || null,
